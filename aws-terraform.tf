1cae1fbdf4a44ae829fe8194db61b46b234b1918
Miniplop <kevinj@theodo.fr>
2018-12-21 22:46:01 +0100

    Add ECS Creation with the deployement task definition

--- aws-terraform.tf
+++ aws-terraform.tf
@@ -10,18 +10,24 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-resource "aws_instance" "example" {
+###################
+# EC2 instance
+###################
+resource "aws_instance" "ec2" {
   ami           = "ami-047bb4163c506cd98"
   instance_type = "t2.micro"
   tags {
     Name = "HelloWorld"
   }
 
-  security_groups = ["example-sg"]
+  security_groups = ["ec2-sg"]
 }
 
-resource "aws_security_group" "example-sg" {
-  name        = "example-sg"
+###################
+# Security group
+###################
+resource "aws_security_group" "ec2-sg" {
+  name        = "ec2-sg"
   description = "Test security group"
 
   ingress {
@@ -30,4 +36,45 @@ resource "aws_security_group" "example-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+###################
+# ECR Repository
+###################
+resource "aws_ecr_repository" "ecr" {
+  name = "kevin"
+}
+
+###################
+# ECS Cluster
+###################
+resource "aws_ecs_cluster" "ecs-cluster" {
+  name = "kevin-cluster"
+}
+
+###################
+# ECS Task Definition
+###################
+
+# See this issue for injecting ecr repository in container definition
+# https://github.com/terraform-providers/terraform-provider-aws/issues/632
+
+resource "aws_ecs_task_definition" "kevin-tasks" {
+  family = "kevin-tasks"
+  network_mode = "bridge"
+  container_definitions = <<DEFINITION
+  [
+    {
+      "name": "kevin-deployement",
+      "image": "${aws_ecr_repository.ecr.repository_url}:latest",
+      "essential": true,
+      "memoryReservation": 512,
+      "cpu": 256,
+      "portMappings": [{
+        "containerPort": 80,
+        "hostPort": 0
+      }]
+    }
+  ]
+  DEFINITION
 }
