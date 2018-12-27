02faf6e10c721c62346896b02c55413512a32633
Miniplop <kevinj@theodo.fr>
2018-12-27 22:08:52 +0100

    Add iam role

--- aws-terraform.tf
+++ aws-terraform.tf
@@ -11,11 +11,6 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-resource "aws_key_pair" "ec2-key" {
-  key_name   = "ec2-key"
-  public_key = "${var.ssh_public_key}"
-}
-
 data "template_file" "config-ecs" {
   template = <<EOF
     #!/bin/bash
@@ -23,19 +18,60 @@ data "template_file" "config-ecs" {
   EOF
 }
 
+data "aws_iam_policy_document" "task-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "ec2-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
 ###################
-# EC2 instance
+# AWS Key pair
 ###################
-resource "aws_instance" "ec2" {
-  ami = "ami-c91624b0"
+resource "aws_key_pair" "ec2-key" {
+  key_name   = "ec2-key"
+  public_key = "${var.ssh_public_key}"
+}
+
+###################
+# Autoscaling group
+###################
+resource "aws_launch_configuration" "app_conf" {
+  name   = "app_config"
+  image_id      = "ami-c91624b0"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.ec2-key.key_name}"
-  tags {
-    Name = "HelloWorld"
-  }
-
-  security_groups = ["ec2-sg"]
+  security_groups = ["${aws_security_group.ec2-sg.name}"]
   user_data = "${base64encode(data.template_file.config-ecs.rendered)}"
+  iam_instance_profile = "${aws_iam_instance_profile.ec2_profile.arn}"
+}
+
+resource "aws_autoscaling_group" "app_autoscaling" {
+  availability_zones = ["eu-west-1a"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+  launch_configuration = "${aws_launch_configuration.app_conf.name}"
+  tags = [{
+    key = "Name"
+    value = "HelloWorld"
+    propagate_at_launch = true
+  }]
 }
 
 ###################
@@ -58,6 +94,36 @@ resource "aws_security_group" "ec2-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+###################
+# IAM Roles
+###################
+resource "aws_iam_role" "task_role" {
+  name = "task_role"
+  path =  "/"
+  assume_role_policy = "${data.aws_iam_policy_document.task-role-policy.json}"
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+  path =  "/"
+  assume_role_policy = "${data.aws_iam_policy_document.ec2-role-policy.json}"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "test_profile"
+  role = "${aws_iam_role.ec2_role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "attach-ecs-policy-to-task-role" {
+  role       = "${aws_iam_role.task_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+}
+
+resource "aws_iam_role_policy_attachment" "attach-ecs-policy-to-ec2-role" {
+  role       = "${aws_iam_role.ec2_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
 
 ###################
@@ -84,6 +150,7 @@ resource "aws_ecs_cluster" "ecs-cluster" {
 resource "aws_ecs_task_definition" "deployment-task" {
   family = "deployment-task"
   network_mode = "bridge"
+  task_role_arn = "${aws_iam_role.task_role.arn}"
   container_definitions = <<DEFINITION
   [
     {
@@ -111,4 +178,8 @@ resource "aws_ecs_service" "app" {
   desired_count   = 1
   deployment_maximum_percent = 200
   deployment_minimum_healthy_percent = 100
+}
+
+output "ecs_repository" {
+  value = "${aws_ecr_repository.ecr.repository_url}"
 }
