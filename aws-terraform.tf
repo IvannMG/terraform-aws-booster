1e83ff81543988c608ce592181faa08c7214fafa
Miniplop <kevinj@theodo.fr>
2019-01-17 10:17:27 +0100

    Allow tcp connection from the ec2 instance

--- aws-terraform.tf
+++ aws-terraform.tf
@@ -94,6 +94,13 @@ resource "aws_security_group" "ec2-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 ###################
